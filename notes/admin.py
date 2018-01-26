88a525fca92aa82ba4fb893e68fe49724383086e
Dale Seo <daeyoung@amazon.com>
2018-01-26 13:01:53 -0800

    temp model post

--- notes/admin.py
+++ notes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Note
+from .models import Note, Post
 
 class NoteAdmin(admin.ModelAdmin):
     list_display = ('title', 'date', 'user')
@@ -7,3 +7,4 @@ class NoteAdmin(admin.ModelAdmin):
     search_fields = ['title', 'memo']
 
 admin.site.register(Note, NoteAdmin)
+admin.site.register(Post)
--- notes/models.py
+++ notes/models.py
@@ -8,3 +8,22 @@ class Note(models.Model):
 
     def __str__(self):
         return self.title
+
+class Post(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.DO_NOTHING)
+    date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True, null=True)
+    kind = models.CharField(max_length=31, blank=True, null=True)
+    url = models.CharField(max_length=255, blank=True, null=True)
+    file = models.CharField(max_length=255, blank=True, null=True)
+    hits = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+# class PostTag(models.Model):
+#     title = models.CharField(max_length=45)
+#
+#     def __str__(self):
+#         return self.title
