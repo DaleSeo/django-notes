f0bef45172e89989dc181ceeb7a0d4995146fada
Dale Seo <daeyoung@amazon.com>
2018-01-25 21:02:47 -0800

    squash migrations

--- notes/models.py
+++ notes/models.py
@@ -3,5 +3,8 @@ from django.db import models
 class Note(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=255)
-    memo = models.TextField(blank=True)
+    memo = models.TextField(blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
